037e023042cff891c27db3f4205164b22ccf35d2
Ashish Kumar <ashish73e@gmail.com>
2025-02-25 04:37:22 +0530

    added backend url

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const winPatterns = [
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
